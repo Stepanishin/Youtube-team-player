5187794e3576214fab53645b2d2381d35dd03e1e
Evgenii Stepanishin <evgenii.stepanishin@agiledrop.com>
2023-11-03 15:38:22 +0100

    Update replaceHtmlEntities func

--- fe/src/utils/helpers/replaceHtmlQuotes.ts
+++ fe/src/utils/helpers/replaceHtmlQuotes.ts
@@ -1,3 +1,10 @@
-export default function replaceHtmlQuotes(str: string) {
-  return str.replace(/&quot;/g, '"');
+export default function replaceHtmlEntities(str: string) {
+  return str
+    .replace(/&quot;/g, '"')
+    .replace(/&amp;/g, "&")
+    .replace(/&lt;/g, "<")
+    .replace(/&gt;/g, ">")
+    .replace(/&apos;/g, "'")
+    .replace(/&nbsp;/g, " ")
+    .replace(/&#39;/g, "'");
 }
